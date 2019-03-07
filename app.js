cfbcadb90d51a2568685d07725545d9d060d842c
Ivan <4esoquevedoi@gmail.com>
2019-03-07 14:04:29 +0100

    modules hbs, mongoose

--- app.js
+++ app.js
@@ -1,17 +1,26 @@
-const createError = require('http-errors');
+'use strict';
+
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const mongoose = require('mongoose');
+const hbs = require('hbs');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 
 const app = express();
 
+mongoose.connect('mongodb://localhost/esplit', {
+  keepAlive: true,
+  useNewUrlParser: true,
+  reconnectTries: Number.MAX_VALUE
+});
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'hbs');
 
 app.use(logger('dev'));
 app.use(express.json());
