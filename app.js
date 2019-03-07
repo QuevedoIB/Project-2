80922766f9cef656ac4786e5c53737bd08df6ed4
Ivan <4esoquevedoi@gmail.com>
2019-03-07 16:56:23 +0100

    logout logic added

--- app.js
+++ app.js
@@ -43,6 +43,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req, res, next) => {
+  app.locals.currentUser = req.session.currentUser;
+  next();
+});
+
 app.use('/', indexRouter);
 app.use('/auth', authRouter);
 
--- routes/auth.js
+++ routes/auth.js
@@ -6,19 +6,19 @@ const saltRounds = 10;
 
 /* SIGNUP */
 router.get('/signup', (req, res, next) => {
-  res.render('auth/signup',);
+  res.render('auth/signup');
 });
 
 router.post('/signup', async (req, res, next) => {
   const { username, name, password } = req.body;
-  const user = { username, name, password };  
+  const user = { username, name, password };
   if (!name || !username || !password) {
     // flash (make sure you have all the fields)
-    res.redirect('/auth/signup');     
+    res.redirect('/auth/signup');
     return;
-  }     
+  }
   try {
-    const result = await User.findOne({username})
+    const result = await User.findOne({ username })
     if (result) {
       // the username is taken
       res.redirect('/auth/signup');
@@ -26,52 +26,64 @@ router.post('/signup', async (req, res, next) => {
       const salt = bcrypt.genSaltSync(saltRounds);
       const hashedPassword = bcrypt.hashSync(password, salt);
       const newUser = {
-        username, 
+        username,
         name,
         password: hashedPassword
       };
       const createdUser = await User.create(newUser);
-      console.log(newUser);
       res.redirect('/profile');
-    }    
-  } catch(err) {
+    }
+  } catch (err) {
     next(err);
-  }      
+  }
 });
 
 /* LOGIN */
 router.get('/login', (req, res, next) => {
-  res.render('auth/login',);
+  if (req.session.currentUser) {
+    res.redirect('/');
+    return;
+  }
+  res.render('auth/login');
 });
 
 router.post('/login', async (req, res, next) => {
   const { username, password } = req.body;
   if (!username || !password) {
     // flash (make sure you have all the fields)
-    res.redirect('/auth/signup');     
+    res.redirect('/auth/signup');
   } else {
     try {
-      const user = await User.findOne({username});
+      const user = await User.findOne({ username });
       if (!user) {
         // flash username or password incorrect
         res.redirect('/auth/login');
         return;
       }
-      if (password === user.password) {
+      if (bcrypt.compareSync(password, user.password)) {
         // guardar la sesion
         req.session.currentUser = user;
-        res.redirect('/profile');
+        console.log(req.session.currentUser);
+        res.redirect('/');
       } else {
         // flash username or password incorrect
         // redirigir
         res.redirect('/auth/login');
       }
-      
-    } catch(err) {
+
+    } catch (err) {
       next(err);
-    }    
+    }
   }
-  
+
+  router.post('/logout', (req, res, next) => {
+    if (!req.session.currentUser) {
+      res.redirect('/');
+      return;
+    }
+    delete req.session.currentUser;
+  })
+
 });
 
 
