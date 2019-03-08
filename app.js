a9538ea8c1f442d4f48eff4bd0e2d76f3e8aa6df
Ivan <4esoquevedoi@gmail.com>
2019-03-08 16:42:21 +0100

    heroku mlab deploy

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 'use strict';
-
+require('dotenv').config();
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
@@ -29,7 +29,7 @@ app.use(session({
   }
 }));
 
-mongoose.connect('mongodb://localhost/esplit', {
+mongoose.connect(process.env.MONGODB_URI, {
   keepAlive: true,
   useNewUrlParser: true,
   reconnectTries: Number.MAX_VALUE
