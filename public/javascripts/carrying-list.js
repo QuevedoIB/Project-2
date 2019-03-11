229e825fe2a03240360146ea6d562b68c0c49de5
Ivan <4esoquevedoi@gmail.com>
2019-03-11 21:17:42 +0100

    fixed white screen event list click bug

--- public/javascripts/carrying-list.js
+++ public/javascripts/carrying-list.js
@@ -2,10 +2,12 @@
 
 const main = () => {
   document.addEventListener('click', (event) => {
-    if (event.target.childNodes[0].className === 'hide') {
-      event.target.childNodes.forEach(child => { child.className = 'show'; });
-    } else {
-      event.target.childNodes.forEach(child => { child.className = 'hide'; });
+    if (event.target.className === 'attendees') {
+      if (event.target.childNodes[0].className === 'hide') {
+        event.target.childNodes.forEach(child => { child.className = 'show'; });
+      } else {
+        event.target.childNodes.forEach(child => { child.className = 'hide'; });
+      }
     }
   });
 };
