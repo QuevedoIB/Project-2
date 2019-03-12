55dbb7387af302dc26f42965f7c198d8bacf4be8
Adriana <adrirx5@gmail.com>
2019-03-12 16:52:37 +0100

    css and js event details

---
+++ public/javascripts/form.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const mainForm = () => {
+  const displayTab = (event, button) => {
+    const selectedForm = document.querySelector('.add-item');
+    selectedForm.removeAttribute('id');
+  };
+  const selectedButton = document.querySelector('.add');
+  selectedButton.addEventListener('click', (event) => {
+    displayTab(event, selectedButton);
+  });
+};
+
+window.addEventListener('load', mainForm);
