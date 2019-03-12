84c6a711b527a4024b1d03ae8a11de0c5de37318
Ivan <4esoquevedoi@gmail.com>
2019-03-12 21:20:47 +0100

    changed layout/index google signup

---
+++ public/javascripts/autocomplete-location.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const main = () => {
+  var geocoder = new MapboxGeocoder({
+    accessToken: 'pk.eyJ1IjoiaXZhbm1hcHMiLCJhIjoiY2pzeDNkZHo0MGU2ZjQ1bzV3ZGExNXRmMCJ9.Yc4_1JYlXjBEZ-mXzuETgA'
+  });
+
+  document.getElementById('geocoder').appendChild(geocoder.onAdd(map));
+};
+
+window.addEventListener('load', main);
