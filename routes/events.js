a8e5627189de550f0ba0ae558b38363c912233ab
Ivan <4esoquevedoi@gmail.com>
2019-03-13 21:40:29 +0100

    changed events list display

--- routes/events.js
+++ routes/events.js
@@ -16,6 +16,7 @@ router.get('/', requireLogged, async (req, res, next) => {
   const currentDate = new Date().toISOString();
   try {
     const owned = await Events.find({ owner: user._id });
+    console.log(owned);
     const participating = await Events.find({ attendees: { '$in': [user._id] } });
     const finished = await Events.find({ date: { $lt: currentDate } });
     const events = { owned, participating, finished };
