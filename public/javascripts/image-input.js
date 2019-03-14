8e80ab196b77934a0bc4a98649e82bdb6166b5b4
Ivan <4esoquevedoi@gmail.com>
2019-03-14 15:28:25 +0100

    merged all, hide show take picture

--- public/javascripts/image-input.js
+++ public/javascripts/image-input.js
@@ -3,9 +3,15 @@
 const mainInput = () => {
   const displayTab = (event, button) => {
     const selectedForm = document.getElementById('image-form');
-    selectedForm.removeAttribute('class');
+
+    if (selectedForm.className === 'invisible') {
+      selectedForm.className = 'visible';
+    } else {
+      selectedForm.className = 'invisible';
+    }
   };
   const selectedButton = document.querySelector('.add-image');
+
   selectedButton.addEventListener('click', (event) => {
     displayTab(event, selectedButton);
   });
